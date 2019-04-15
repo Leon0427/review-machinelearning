7a6dbccd2cb3adabde99970c5749271347d81cce
Administrator <235151566@qq.com>
2019-04-16 00:03:42 +0800

    add neural networks

---
+++ machine-learning-andrew-ng-2017/neural_networks/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/4/15 22:40
+# @Author  : Leon
+# @Site    : 
+# @File    : __init__.py.py
---
+++ machine-learning-andrew-ng-2017/neural_networks/backPropagation.md
@@ -0,0 +1,4 @@
+### coss funcion
+
+![Alt text](../../imgs/神经网络coss.PNG "optional title")
+
