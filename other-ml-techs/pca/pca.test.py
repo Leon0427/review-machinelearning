e3b075d23a1b6dec2ddbcedfebfeb485e6504c25
梁宵 <liangxiao@sunlands.com>
2019-04-16 10:47:46 +0800

    pca test modified

--- other-ml-techs/pca/pca.test.py
+++ other-ml-techs/pca/pca.test.py
@@ -13,12 +13,24 @@ from sklearn.linear_model import LogisticRegression
 iris_data = load_iris()
 X = iris_data.data
 y = iris_data.target
-pca = PCA(n_components=3)
-X_ = pca.fit_transform(X)
+pca2 = PCA(n_components=3)
+pca3 = PCA(n_components=2)
+pca4 = PCA(n_components=1)
+X2 = pca2.fit_transform(X)
+X3 = pca3.fit_transform(X)
+X4 = pca4.fit_transform(X)
 lr1 = LogisticRegression()
 lr2 = LogisticRegression()
+lr3 = LogisticRegression()
+lr4 = LogisticRegression()
 lr1.fit(X,y)
-lr2.fit(X_,y)
+lr2.fit(X2,y)
+lr3.fit(X3,y)
+lr4.fit(X4,y)
 print y
 print lr1.predict(X)
-print lr2.predict(X_)
+print lr2.predict(X2)
+print lr3.predict(X3)
+print lr4.predict(X4)
+
+print "as we can see, as the n_components goes down, the precession of the model we build with X_i goes down as well"
