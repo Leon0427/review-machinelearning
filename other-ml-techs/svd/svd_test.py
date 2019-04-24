bbfae85cc45a16b97f433a8d17ac240cc6029ed7
梁宵 <liangxiao@sunlands.com>
2019-04-24 15:58:15 +0800

    svm modified

--- other-ml-techs/svd/svd_test.py
+++ other-ml-techs/svd/svd_test.py
@@ -27,16 +27,17 @@ V∈R(k*n)
 if __name__ == '__main__':
     data_shape = (7,4)
     data = np.random.randint(1,10,data_shape)
-    print "the original data: \n%s"%data
+    print ("the original data: \n%s"%data)
     u,sigma,v = svd(data)
-    print "singular values of data: \n%s"%sigma
+    print ("singular values of data: \n%s"%sigma)
+    print("shape of u, sigma, v : {0}, {1}, {2}".format(u.shape, sigma.shape, v.shape))
     sigma_mat = np.zeros(data_shape)
     # only use first 2 singular values
     sigma_mat[0,0] = sigma[0]
     sigma_mat[1,1] = sigma[1]
     sigma_mat[2,2] = sigma[2]
     _data = u.dot(sigma_mat).dot(v)
-    print "data correspond to first 3 singular values: \n%s "%_data
+    print ("data correspond to first 3 singular values: \n%s "%_data)
     # use all singular values
     sigma_mat[3,3] = sigma[3]
-    print "data correspond to all sigular values: \n%s" % u.dot(sigma_mat).dot(v)
+    print ("data correspond to all sigular values: \n%s" % u.dot(sigma_mat).dot(v))
